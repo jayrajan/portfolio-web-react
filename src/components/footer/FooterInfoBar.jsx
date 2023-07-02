fd7239366a9ca3cef8dd3deacdd14666833261d0
jayrajan <jrajan@jprtech.co.uk>
2023-07-02 21:06:14 +0100

    feat: implement footer

---
+++ src/components/footer/FooterInfoBar.jsx
@@ -0,0 +1,15 @@
+import './FooterInfoBar.css'
+
+function FooterInfoBar() {
+    
+    return (
+        <>
+            <div className="footer">
+                <p>Copyright 2023 JWEB. All rights reserved</p>
+            </div>
+        </>
+    );
+
+}
+
+export default FooterInfoBar;
